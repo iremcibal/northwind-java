1ccac078e511ce46f7679216e4eb7fda6f094c31
İrem Balcı <info@irembalci.com>
2022-12-12 22:48:12 +0300

    add global exceptions

--- src/main/java/kodlama/io/northwind/NorthwindApplication.java
+++ src/main/java/kodlama/io/northwind/NorthwindApplication.java
@@ -1,9 +1,18 @@
 package kodlama.io.northwind;
 
+import kodlama.io.northwind.core.exception.BusinessException;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class NorthwindApplication {
@@ -15,5 +24,23 @@ public class NorthwindApplication {
 	@Bean
 	public ModelMapper getModelMapper(){
 		return new ModelMapper();
+	}
+
+	@ExceptionHandler
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public Object handleValidationException(MethodArgumentNotValidException exception){
+		Map<String,String> errors = new HashMap<>();
+
+		for(FieldError fieldError : exception.getBindingResult().getFieldErrors()){
+			errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+		}
+
+		return errors;
+	}
+
+	@ExceptionHandler
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public String handleBusinessException(BusinessException exception){
+		return exception.getMessage();
 	}
 }
